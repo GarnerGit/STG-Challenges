340372d0db2ca007b1e9a05183758398355fad14
GarnerGit <Bryan1.Garner@dish.com>
2020-02-14 15:02:24 -0700

    test commit

--- challenges/Challenge 5/Challenge5.py
+++ challenges/Challenge 5/Challenge5.py
@@ -9,9 +9,9 @@ class Challenge1(unittest.TestCase):
     def tearDown(self):
         self.driver.close()
 
-    def test_challenge5\(self):
-        self.driver.get("https://www.google.com")
-        self.assertIn("Google", self.driver.title)
+    def test_challenge5(self):
+        pass
+
 
     if __name__ == '__main__':
         unittest.main()
